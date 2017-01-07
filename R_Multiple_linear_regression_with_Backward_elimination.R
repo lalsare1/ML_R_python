1cae02f94baa023f04cd513571b70ee3a8d0ccfe
amoolyadlalsare@gmail.com <lalsare.1@osu.edu>
2017-01-06 23:09:11 -0600

    Multiple Linear Regression Model Using R

---
+++ R_Multiple_linear_regression_with_Backward_elimination.R
@@ -0,0 +1,41 @@
+#Importing the dataset
+dataset = read.csv('50_Startups.csv')
+
+#Encoding categorical data
+
+dataset$State = factor(dataset$State, 
+                         levels = c('New York', 'California', 'Florida'), 
+                         labels = c(1, 2, 3))
+
+
+#Splitting data set into training set and test set
+#install.packages('caTools')
+library(caTools)
+set.seed(123)
+split = sample.split(dataset$Profit, SplitRatio = 0.8)
+training_set = subset(dataset, split== TRUE)
+test_set = subset(dataset, split== FALSE)
+
+#Fitting multiple linear regression model
+regressor = lm(formula = Profit ~ .,
+               data = training_set)
+
+#Predicting the test set result
+Y_pred = predict(regressor, newdata = test_set)
+
+#Building the optimal model using Backward Elimination
+regressor = lm(formula = Profit ~ R.D.Spend + Administration + Marketing.Spend + State,
+               data = dataset)
+summary(regressor)
+
+regressor = lm(formula = Profit ~ R.D.Spend + Administration + Marketing.Spend,
+               data = dataset)
+summary(regressor)
+
+regressor = lm(formula = Profit ~ R.D.Spend + Marketing.Spend,
+               data = dataset)
+summary(regressor)
+
+regressor = lm(formula = Profit ~ R.D.Spend,
+               data = dataset)
+summary(regressor)
