71152a935ac89a9733477cd812c2621a19546c78
amoolyadlalsare@gmail.com <lalsare.1@osu.edu>
2017-01-12 13:31:04 -0600

    K-Means Clustering Model in R

---
+++ R_k_means_clustering_model.R
@@ -0,0 +1,30 @@
+#Importing the dataset
+dataset = read.csv('Mall_Customers.csv')
+X = dataset[4:5]
+
+#Using the elbow method to find the optimal number of clusters
+set.seed(6)
+wcss = vector()
+for (i in 1:10) wcss[i] = sum(kmeans(X, i)$withinss)
+plot(1:10, wcss, type = 'b', main = paste('Cluster of Clients'),
+     xlab = "Number of Clusters", ylab = "WCSS")
+
+#Applying k-means to the client dataset
+set.seed(29)
+k_means = kmeans(X, 5, iter.max = 300, nstart = 10)
+
+#Visualizing the clusters
+library(cluster)
+clusplot(X,
+         clus = k_means$cluster,
+         lines = 0,
+         shade = TRUE,
+         color = TRUE, 
+         labels = 2,
+         plotchar = FALSE,
+         span = TRUE,
+         main = paste('Clusters of clients'),
+         xlab = "Annual Income",
+         ylab = "Spending Score")
+         
+         
