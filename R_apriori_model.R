2e590cf25619d9aa0a6fee613856a84d51ed8c65
amoolyadlalsare@gmail.com <lalsare.1@osu.edu>
2017-01-13 17:58:31 -0600

    Apriori Algorithm Model in R

---
+++ R_apriori_model.R
@@ -0,0 +1,18 @@
+#Apriori algorithm
+
+#Importing the dataset
+#install.packages('arules')
+library(arules)
+dataset = read.csv('Market_Basket_Optimisation.csv', header = FALSE)
+dataset = read.transactions('Market_Basket_Optimisation.csv', sep = ',', rm.duplicates = TRUE)
+summary(dataset)
+itemFrequencyPlot(dataset, topN = 20)
+
+#Training apriori on the dataset
+rules = apriori(data = dataset, parameter = list(support = 0.004,
+                                                 confidence = 0.2))
+
+#Visualizing the results
+inspect(sort(rules, by= 'lift')[1:10])
+
+
