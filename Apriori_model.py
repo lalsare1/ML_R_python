653b70144773bd1979d8ecf9d705d8afddb2174b
amoolyadlalsare@gmail.com <lalsare.1@osu.edu>
2017-01-13 18:40:00 -0600

    Apriori Model in Python

---
+++ Apriori_model.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jan 13 02:41:39 2017
+
+@author: AmoolyaD
+"""
+
+#Apriori Algorithm based modeling for a grocery store in South France
+#Importing the libraries
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+#Importing the dataset
+dataset = pd.read_csv('Market_Basket_Optimisation.csv', header = None)
+transactions = []
+for i in range (0, 7501):
+    transactions.append([str(dataset.values[i, j]) for j in range(0, 20)])
+
+#Training Apriori on the dataset
+from apyori import apriori
+rules = apriori(transactions, min_support = 0.003, min_confidence = 0.2, min_length = 2, min_lift = 3)
+
+#Visualizing the results
+results = list(rules)
+
+
+
