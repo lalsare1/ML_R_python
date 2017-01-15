ae39ee901037c0c117bc45193065ce881fc134f9
amoolyadlalsare@gmail.com <lalsare.1@osu.edu>
2017-01-15 01:46:34 -0600

    Upper Confidence Bound Algorithm for Most Selected Ad in Python

---
+++ upper_confidence_bound_python.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Jan 14 10:43:29 2017
+
+@author: AmoolyaD
+"""
+
+#Upper Confidence Bound - Reinforcement Learning
+
+#Importing the libraries
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
