d1068a350c593814fdbdc01ca3692041ed451e5e
amoolyadlalsare@gmail.com <lalsare.1@osu.edu>
2017-01-21 16:53:08 -0600

    Artificial Neural Network Model on Bank Data using Python

--- artificial_neural_networks_python.py
+++ artificial_neural_networks_python.py
@@ -41,14 +41,32 @@ X_test = sc.transform(X_test)
 
 #Now Lets make ANN
 # Importing the Keras libraries and required packages
+import tensorflow as tf
 import keras
 from keras.models import Sequential
 from keras.layers import Dense
 
 # Create your classifier here
+classifier = sequential()
+
+#Adding the input layer and the first hidden layer
+classifier.add(Dense(output_din = 6, init = 'uniform', activation = 'relu', input_dim = 11))
+
+#Adding the second hidden layer
+classifier.add(Dense(output_dim= 6, init= 'uniform', activation= 'relu'))
+
+#Adding the output layer
+classifier.add(Dense(output_dim= 1, init= 'uniform', activation= 'sigmoid'))
+
+#Compiling the ANN
+classifier.compile(optimizer = 'adam', loss = 'binary_crossentropy', metrics = ['accuracy'])
+
+#Fitting the ANN model to the data
+classifier.fit(X_train, Y_train, batch_size = 10, nb_epoch = 100)
 
 # Predicting the Test set results
 y_pred = classifier.predict(X_test)
+y_pred = (y_pred > 0.5)
 
 # Making the Confusion Matrix
 from sklearn.metrics import confusion_matrix
